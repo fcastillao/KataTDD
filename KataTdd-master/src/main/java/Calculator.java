b7df7aa42b36e70c90d5d3f93353db8fa610feec
BuildTools <unconfigured@null.spigotmc.org>
2017-01-31 22:45:47 -0500

    format crt shft F

--- KataTdd-master/src/main/java/Calculator.java
+++ KataTdd-master/src/main/java/Calculator.java
@@ -1,39 +1,27 @@
 public class Calculator {
 
-	public int Add(String numbers){
-		
-		
+	public int Add(String numbers) {
+
 		if (numbers.equals("")) {
-			return 0;	
-			 
+			return 0;
+
 		} else {
-			int s=0;
+			int s = 0;
 			char ch;
 			int x;
-			for(x=0;x<numbers.length();x++){
-				
-				if(Character.isDigit(numbers.charAt(x))){
-					ch=numbers.charAt(x);
-					
-					s=s+Character.getNumericValue(ch);
+			for (x = 0; x < numbers.length(); x++) {
+
+				if (Character.isDigit(numbers.charAt(x))) {
+					ch = numbers.charAt(x);
+
+					s = s + Character.getNumericValue(ch);
 				}
-				
-				
-				
+
 			}
 			return s;
-			
-			
-			}	 
-			
-			
-					  
-			
-				}
-				
-			
-		
-		}
-		
-	
 
+		}
+
+	}
+
+}
--- KataTdd-master/src/test/java/KataCalculator.java
+++ KataTdd-master/src/test/java/KataCalculator.java
@@ -4,76 +4,79 @@ import org.junit.Test;
 public class KataCalculator {
 
 	@Test
-	public void emptyStringShouldReturn0(){
+	public void emptyStringShouldReturn0() {
 		Calculator calculator = new Calculator();
 		int result = calculator.Add("");
 		Assert.assertEquals(0, result);
 	}
-	
+
 	@Test
-	public void fiveStringShouldReturn5(){
+	public void fiveStringShouldReturn5() {
 		Calculator calculator = new Calculator();
 		int result = calculator.Add("5");
 		Assert.assertEquals(5, result);
 	}
-	
+
 	@Test
-	public void threeStringShouldReturn3(){
+	public void threeStringShouldReturn3() {
 		Calculator calculator = new Calculator();
 		int result = calculator.Add("3");
 		Assert.assertEquals(3, result);
 	}
-	
+
 	@Test
-	public void oneStringShouldReturn1(){
+	public void oneStringShouldReturn1() {
 		Calculator calculator = new Calculator();
 		int result = calculator.Add("1");
 		Assert.assertEquals(1, result);
 	}
-	
+
 	@Test
-	public void twoStringShouldReturn2(){
+	public void twoStringShouldReturn2() {
 		Calculator calculator = new Calculator();
 		int result = calculator.Add("2");
 		Assert.assertEquals(2, result);
 	}
+
 	@Test
-	public void fourStringShouldReturn4(){
+	public void fourStringShouldReturn4() {
 		Calculator calculator = new Calculator();
 		int result = calculator.Add("4");
 		Assert.assertEquals(4, result);
 	}
-	
+
 	@Test
-	public void sixStringShouldReturn6(){
+	public void sixStringShouldReturn6() {
 		Calculator calculator = new Calculator();
 		int result = calculator.Add("6");
 		Assert.assertEquals(6, result);
 	}
-	
-	//pruebas de dos numeros
-	@Test 
-	public void dosMasDosEs4(){
+
+	// pruebas de dos numeros
+	@Test
+	public void dosMasDosEs4() {
 		Calculator calculator = new Calculator();
 		int result = calculator.Add("2,2");
 		Assert.assertEquals(4, result);
 	}
-	@Test 
-	public void TresMasTresEs6(){
+
+	@Test
+	public void TresMasTresEs6() {
 		Calculator calculator = new Calculator();
 		int result = calculator.Add("3,3");
 		Assert.assertEquals(6, result);
 	}
-	//tresnumeros
-	@Test 
-	public void dosDosCuatroEs8(){
+
+	// tresnumeros
+	@Test
+	public void dosDosCuatroEs8() {
 		Calculator calculator = new Calculator();
 		int result = calculator.Add("2,2,4");
 		Assert.assertEquals(8, result);
 	}
-	
-	@Test 
-	public void sieteMenos3Mas1Es5(){
+
+	@Test
+	public void sieteMenos3Mas1Es5() {
 		Calculator calculator = new Calculator();
 		int result = calculator.Add("7,-3,1");
 		Assert.assertEquals(5, result);
